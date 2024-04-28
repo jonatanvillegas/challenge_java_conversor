073f35a38c96c7ac92268ed46a473df8f0f5137e
janatanvillegas <villegasjoncena7@gmail.com>
2024-04-28 09:37:12 -0600

    subiendo proyecto completo

---
+++ src/com/Jonatan/convertidor/calculos/ConsultaDivisa.java
@@ -0,0 +1,43 @@
+package com.willmer.convertidor.calculos;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.willmer.convertidor.modelos.CambioPar;
+import com.willmer.convertidor.modelos.Divisa;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaDivisa {
+    public Divisa calculaDivisa(String baseCode, String targetCode, double monto){
+        String apiKey = "COPIA_TU_API_KEY_AQUI";
+        String direccion = "https://v6.exchangerate-api.com/v6/"+apiKey+"/pair/"+baseCode+"/"+targetCode+"/"+monto;
+        Gson gson= new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+            //hacer un response ********************
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            //System.out.println(json);
+            CambioPar cambioDivisa = gson.fromJson(json, CambioPar.class);
+            Divisa divisaConvertida = new Divisa(cambioDivisa);
+            return divisaConvertida;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+}
+
